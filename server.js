8f93ef62958d43ae06738292bca0981bb9491673
Gustavo Freitas <117666580+Gustavofreitasr@users.noreply.github.com>
2024-12-31 08:40:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// Pacote do MySQL para rodar com o Node.js
 const mysql = require('mysql2');
 require('dotenv').config();
 
+
+// Conexão com o meu banco de dados criado no Mysql
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -8,6 +11,7 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// Função para informar sobre a conexão ter tido sucesso ou não
 connection.connect((err) => {
     if (err) {
         console.error('Erro ao conectar ao MySQL:', err);
@@ -16,4 +20,5 @@ connection.connect((err) => {
     console.log('Conexão com MySQL estabelecida.');
 });
 
+// Responsável por liberar que outros arquivos possam importar e interagir com o DB
 module.exports = connection;
